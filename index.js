67a09b219727c43f939a70e3ef2866d3c8d44ddb
Corey Ghilarducci <cghilarducci@gmail.com>
2017-02-21 21:27:23 -0800

    finished inputs working on generating JWT

--- index.js
+++ index.js
@@ -7,22 +7,26 @@ var jwt = require('jsonwebtoken'),
 	colors = require('colors'),
 	argv = require('minimist')(process.argv.slice(2));
 
-function JWTinputs(client_id, client_secret, ent_or_user_id, box_sub_type, password){
+function PayLoadInputs(client_id, ent_or_user_id, box_sub_type){
 	this.client_id = client_id;
 	this.ent_or_user_id = ent_or_user_id;
 	this.box_sub_type = box_sub_type;
-	this.password = password;
 	this.exp = generateExp();
 	this.jti = generateJTI();
 	this.aud = "https://api.box.com/oauth2/token"
 };
+
+function KeyInputs(filePath, keyId){
+	this.privateKey = fs.readFileSync(path);
+	this.publicKeyId = keyId;
+}
 
 // generates random string for JTI
 function generateJTI(){
 	return crypto.randomBytes(20).toString('hex');
 };
 
-// generates the time in which the JWT will expire
+// generates the time for when the JWT will expire (59 seconds after created)
 function generateExp(){
 	var currentDate = new Date();
 	currentDate = currentDate.getTime();
@@ -36,8 +40,26 @@ function generateJWT(inputs){
 };
 
 function requestForAccessToken(signed_JWT, client_secret, client_id){
+	var options = {
+		method: "POST",
+		url: "https://api.box.com/oauth2/token",
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded'
+		},
+		form: {
+			grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+			client_id: client_id,
+			client_secret: client_secret,
+			assertion: signed_JWT
+		}
+	};
 
-}
+	request(options, responseHandler);
 
-var inputs = new JWTinputs();
+	function responseHandler(error, response, body){
+		if(error) throw new Error(error);
+	};
+};
+
+var inputs = new PayLoadInputs();
 console.log(inputs)
